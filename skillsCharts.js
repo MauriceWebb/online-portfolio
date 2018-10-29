e9db96390dde29daff533cb4552c70ab5ab9b064
MauriceWebb <maurice.webb91@gmail.com>
2018-10-29 00:41:05 -0400

    push updated html & css files to new repo to replace old portfolio

---
+++ skillsCharts.js
@@ -0,0 +1,29 @@
+$(function() {
+  $('.chart-1').easyPieChart({
+    size: 150,
+    barColor: '#EB653B',
+    scaleColor: false,
+    lineWidth: 20,
+    trackColor: '333',
+    lineCap: 'circle',
+    animate: 2000
+  });
+  $('.chart-2').easyPieChart({
+    size: 150,
+    barColor: '#3692CB',
+    scaleColor: false,
+    lineWidth: 20,
+    trackColor: '333',
+    lineCap: 'circle',
+    animate: 4000
+  });
+  $('.chart-3').easyPieChart({
+    size: 150,
+    barColor: '#37B263',
+    scaleColor: false,
+    lineWidth: 20,
+    trackColor: '333',
+    lineCap: 'circle',
+    animate: 6000
+  });
+});
