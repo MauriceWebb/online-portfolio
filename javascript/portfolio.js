f80f31d07fbd3d27aeda0a03ee368f4f8c9f281b
MauriceWebb <maurice.webb91@gmail.com>
2019-03-25 21:56:02 -0400

    refactor template literals  & category.logos.images

--- javascript/portfolio.js
+++ javascript/portfolio.js
@@ -7,27 +7,35 @@ var $catTitle = $('#category_title');
 // portfolio contents
 var category = {
     logos: {
-        images: []
+        images: [
+            'portfolio_images/logos/art-0.png',
+            'portfolio_images/logos/art-1.png',
+            'portfolio_images/logos/art-2.png',
+            'portfolio_images/logos/art-3.png',
+            'portfolio_images/logos/art-4.png',
+            'portfolio_images/logos/art-5.png'
+        ]
     },
     illustrations: {
         images: [
+            'portfolio_images/illustrations/art-0.png',
             'portfolio_images/illustrations/art-1.png',
             'portfolio_images/illustrations/art-2.png',
             'portfolio_images/illustrations/art-3.png',
             'portfolio_images/illustrations/art-4.png',
-            'portfolio_images/illustrations/art-5.png',
-            'portfolio_images/illustrations/art-6.png'
+            'portfolio_images/illustrations/art-5.png'
         ]
     },
     prints: {
         images: []
     },
-    'UI/UX': {
+    'ui/ux': {
         images: []
     }
 };
 var numOfCats = Object.keys(category).length;
 var onCat = 0;
+
 
 // click events
 $prev.on('click', function (event) {
@@ -48,10 +56,14 @@ $next.on('click', function (event) {
 });
 
 // functions
-function render_gallery() {
+function getCatName() {
     // change gallery title
     var catName = Object.keys(category)[onCat];
     $catTitle.text(catName);
+}
+function render_gallery() {
+    // change gallery title
+    getCatName();
 // populate gallery
     // first empty current gallery
     $gallery.empty();
@@ -60,7 +72,17 @@ function render_gallery() {
         for (var i = 0; i < category.illustrations.images.length; i++) {
         $gallery.append(`
         <div class="col-sm-12 col-md-12 col-lg-4 portfolio_pic m-2">
-		    <img class="" src="/portfolio_images/illustrations/art-`+i+`.png" alt="">
+		    <img class="" src=${category.illustrations.images[i]} alt="">
+		</div>
+        `);
+        }
+    }
+    else if ($catTitle.text() === 'logos') {
+        // console.log('these are the illustrations!');
+        for (var i = 0; i < category.logos.images.length; i++) {
+        $gallery.append(`
+        <div class="col-sm-12 col-md-12 col-lg-4 portfolio_pic m-2">
+            <img class="" src=${category.logos.images[i]} alt="">
 		</div>
         `);
         }
